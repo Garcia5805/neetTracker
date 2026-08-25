35696be7888c2a746c8fed45ae7f2675ca4b9fa5
Fer <fercia400@gmail.com>
2026-08-24 22:25:04 -0500

    fix: now skips questions that require Pro Access

--- scraper/main.py
+++ scraper/main.py
@@ -6,36 +6,49 @@ def main():
 
         browser = p.firefox.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://neetcode.io/problems/concatenation-of-array/question")
+        page.goto("https://neetcode.io/problems/concatenation-of-array/question?list=allNC")
         
         p.selectors.set_test_id_attribute("data-tooltip")
 
 
         start = time.time()
-        while time.time() - start < 10:     
-            page.wait_for_timeout(2000)  
-            h1 = page.locator("h1.problem-title")
-            name = h1.inner_text()
+        while time.time() - start < 20:   
 
-            diff_loc = page.locator('[class^="difficulty-pill"]')
-            diff_text = diff_loc.inner_text()
+            page.wait_for_timeout(1500)
+            pro = page.get_by_role("button", name="Get Pro Access")
+            pro_visible = pro.is_visible()
 
-            top_loc = page.locator(".hint-accordion", has_text="Topics")
-            top_loc.click()
-            top_loc = top_loc.locator('[class^="company-tags-container"]')
-            top_text = top_loc.inner_text()
+            if(pro_visible):
+                print("Skip")
+                locator = page.get_by_test_id("Next Question")
+                locator.hover()
+                page.wait_for_timeout(500)
+                locator.click()
+                continue
+            else:
 
-            page_url = page.url
+                h1 = page.locator("h1.problem-title")
+                name = h1.inner_text()
 
-            acc_loc = page.locator(".acceptance-value")
-            acc_rate = acc_loc.inner_text()
+                diff_loc = page.locator('[class^="difficulty-pill"]')
+                diff_text = diff_loc.inner_text()
 
-            
-            print(name + " " + diff_text + " " + top_text + " " + page_url + " " + acc_rate)
+                top_loc = page.locator(".hint-accordion", has_text="Topics")
+                top_loc.click()
+                top_loc = top_loc.locator('[class^="company-tags-container"]')
+                top_text = top_loc.inner_text()
 
-            locator = page.get_by_test_id("Next Question")
-            locator.hover()
-            locator.click()
+                page_url = page.url
+
+                acc_loc = page.locator(".acceptance-value")
+                acc_rate = acc_loc.inner_text()
+
+                    
+                print(name + " " + diff_text + " " + top_text + " " + page_url + " " + acc_rate)
+
+                locator = page.get_by_test_id("Next Question")
+                locator.hover()
+                locator.click()
 
         browser.close()
 
