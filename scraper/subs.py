e7af0b1405d55e6181c7689c2acfd0ed969b0270
Fer <fercia400@gmail.com>
2026-09-17 20:23:57 -0500

    Access problem foreign key by matching URL

--- scraper/subs.py
+++ scraper/subs.py
@@ -1,14 +1,27 @@
 import subprocess
 import os
+from database.db import get_connection
+
 
 from dotenv import load_dotenv
 
 def main():
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    url = "https://neetcode.io/problems/"
+
     load_dotenv()
     path = os.getenv("SUB_PATH")
+
     for filename in os.listdir(path):
+
         file_path = path + "/"+filename
-        print(filename)
+        
+        problem_url =url+filename
+        print(problem_url)
+
         for problem in os.listdir(file_path):
             problem_path = file_path+"/"+problem
             result = subprocess.run(
@@ -18,7 +31,16 @@ def main():
                 check=True
             )
             commit_date = result.stdout.strip()
-            print(problem + " " + commit_date)            
+            cur.execute("""
+                SELECT id
+                FROM problems
+                WHERE url LIKE %s
+            """, (f"%/{filename}/%",))
+            result = cur.fetchone()
+            
+            problem_id = result[0] #foreign key 
+            print(problem_id) 
+
             
 
 if __name__ == '__main__':
